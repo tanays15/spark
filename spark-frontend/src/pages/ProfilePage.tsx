79c89ae46458dd25e2f0d477e56bd0a0125b9672
Adrian Maliackel <amaliack@purdue.edu>
2025-02-22 17:40:33 -0500

    One more

--- spark-frontend/src/pages/ProfilePage.tsx
+++ spark-frontend/src/pages/ProfilePage.tsx
@@ -1,4 +1,3 @@
-import {useEffect} from "react";
 import React, { useEffect } from 'react'
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
