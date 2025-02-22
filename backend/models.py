f3c2e26ae40a704e8dd3c34b99cb3872e8d85d50
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-02-22 16:02:35 -0500

    feat: updating models and relationships from topic and user

--- backend/models.py
+++ backend/models.py
@@ -9,9 +9,10 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     auth0_id = db.Column(db.String(100), unique=True, nullable=False)
     username = db.Column(db.String(100), nullable=False)
-
     records = db.relationship(
-        "Record", back_populates="user", cascade="all, delete-orphan")
+        "Topic", back_populates="User", cascade="all, delete-orphan")
+    records = db.relationship(
+        "Record", back_populates="User", cascade="all, delete-orphan")
 
     def to_dict(self):
         return {"id": self.id, "username": self.username, "auth0_id": self.auth0_id}
@@ -21,18 +22,23 @@ class Topic(db.Model):
     __tablename__ = 'topics'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    avg_content_score = db.Column(db.Decimal(10, 2), server_default="0")
-    avg_confidence_score = db.Column(db.Decimal(10, 2), server_default="0")
-    avg_score = db.Column(db.Decimal, server_default=0)
-
     records = db.relationship(
         "Record", back_populates="topic", cascade="all, delete-orphan")
 
     def to_dict(self):
+        avg_content = sum(record.contentScore for record in self.records)
+        avg_conf = sum(record.confidenceScore for record in self.records)
+        avg_score = sum(record.score for record in self.records)
+        record_count = len(self.records)
+        if (record_count > 0):
+            avg_content /= record_count
+            avg_conf /= record_count
+            avg_score /= record_count
         return {"id": self.id, "name": self.name,
-                "avg_content_score": self.avg_content_score,
-                "average_confidence_score": self.avg_confidence_score,
-                "avg_score": self.avg_score}
+                "avg_content_score": avg_content,
+                "average_confidence_score": avg_conf,
+                "avg_score": avg_score,
+                "record_count": len(self.records)}
 
 
 class Record(db.Model):
