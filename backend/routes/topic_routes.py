dbd747100805ec9c7eb99551365cff1aea923275
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-02-22 16:04:10 -0500

    feat: cleaning up topic routes

--- backend/routes/topic_routes.py
+++ backend/routes/topic_routes.py
@@ -4,11 +4,6 @@ from models import Record, db, User, Topic
 user_bp = Blueprint("user_bp", __name__)
 
 
-def get_records_per_topic(topic_id):
-    records = Record.query.filter_by(topic_id=topic_id).all()
-    return [record.to_dict() for record in records]
-
-
 @user_bp.route("/topics", methods=["POST"])
 def manage_topic():
     if request.method == "POST":
