0c8976738a54b2d75058e336123ce1a046c93b68
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-02-22 22:44:24 -0500

    somewhat working app

--- spark-frontend/src/App.tsx
+++ spark-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import AuthPage from './pages/AuthPage';
 import MainPage from './pages/MainPage';
 import ProfilePage from './pages/ProfilePage';
 import Results from './pages/Results';
+import Recording from './pages/Recording';
 
 import React from 'react'
 import { ThemeProvider, CssBaseline } from "@mui/material";
@@ -11,14 +12,15 @@ import theme from "./theme"; // Import theme file
 
 function App() {
     return (
-             <Routes>
-         <Route path="/" element={<LandingPage />} />
-         <Route path="/auth" element={<AuthPage />} />
-         <Route path="/main" element={<MainPage />} />
-         <Route path="/profile" element={<ProfilePage />} />
-         <Route path="/logout" element={<Results />} />
-      </Routes>
-   );
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/main" element={<MainPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/logout" element={<Results />} />
+            <Route path='/recording' element={<Recording />} />
+        </Routes>
+    );
 }
 
 export default App;
